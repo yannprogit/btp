81803f416a1a2ca33314268e3b466aff63ebb205
Yann <yann.sady.pro@gmail.com>
2025-06-26 16:21:08 +0200

    👷 add delete images

--- stop.bat
+++ stop.bat
@@ -2,16 +2,19 @@
 echo Stopping API Gateway
 cd api-gateway
 docker-compose down
+docker rmi api-gateway-api_gateway
 cd ..
 
 echo Stopping team service
 cd team-service
 docker-compose down
+docker rmi team-service-team-app
 cd ..
 
 echo Stopping user service
 cd user-service
 docker-compose down
+docker rmi user-service-user-app
 cd ..
 
 echo All services stopped !
