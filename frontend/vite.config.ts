55fff606d3ad62272adfa8f066fdd661cd304708
Yann <yann.sady.pro@gmail.com>
2025-07-03 14:10:20 +0200

    👷 dockerized frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+  host: true,
+  strictPort: true,
+  port: 5173,
+  watch: { usePolling: true },
+  },
 })
