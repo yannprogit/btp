d6246dfb0e01bb1406dad052af36db6fb5c82997
TimotheGury <gury.timothe@gmail.com>
2026-04-09 11:09:39 +0200

    feat: pokemon card type background

--- frontend/src/assets/components/pokemonCard.tsx
+++ frontend/src/assets/components/pokemonCard.tsx
@@ -1,5 +1,26 @@
 import type { Pokemon, Type } from "../interfaces/pokemon";
 
+const typeColors: Record<string, string> = {
+  fire: "#f97316",
+  water: "#3b82f6",
+  grass: "#22c55e",
+  electric: "#facc15",
+  psychic: "#ec4899",
+  ice: "#67e8f9",
+  dragon: "#7c3aed",
+  dark: "#374151",
+  fairy: "#f9a8d4",
+  normal: "#a8a29e",
+  fighting: "#b91c1c",
+  flying: "#60a5fa",
+  poison: "#a855f7",
+  ground: "#92400e",
+  rock: "#78716c",
+  bug: "#84cc16",
+  ghost: "#6b21a8",
+  steel: "#9ca3af",
+};
+
 const PokemonCard = ({
   pokemon,
   onClick
@@ -7,26 +28,38 @@ const PokemonCard = ({
   pokemon: Pokemon;
   onClick: () => void;
 }) => {
+
+  const type1 = pokemon.types[0]?.name;
+  const type2 = pokemon.types[1]?.name;
+
+  const color1 = typeColors[type1] || "#e5e7eb";
+  const color2 = typeColors[type2] || color1;
+
+  const background = type2
+    ? `linear-gradient(135deg, ${color1}, ${color2})`
+    : color1;
+
   return (
     <div
       className="rounded-xl shadow p-4 cursor-pointer"
       onClick={onClick}
+      style={{ background }}
     >
-      <img className="h-45 object-cover mx-auto" src={pokemon.sprite} alt={pokemon.name}></img>
+      <img className="h-45 object-cover mx-auto" src={pokemon.sprite} alt={pokemon.name}/>
       <div className="p-4">
         <p className="text-lg font-semibold text-gray-800 text-center">
-        {pokemon.name}
+          {pokemon.name}
         </p>
         <div className="flex justify-center justify-between">
-        {pokemon.types.map((type:Type) => (
-        <img
-          key={type.name}
-          src={`/assets/images/types/${type.name}.png`}
-          alt={type.name}
-          title={type.name}
-          className="w-12 mx-1"
-        />
-        ))}
+          {pokemon.types.map((type:Type) => (
+            <img
+              key={type.name}
+              src={`/assets/images/types/${type.name}.png`}
+              alt={type.name}
+              title={type.name}
+              className="w-12 mx-1"
+            />
+          ))}
         </div>
       </div>
     </div>
